6e0e913e8125b4865a969c950ef26d9dce35b44d
revellan <climb.simonschmidt@web.de>
2025-07-25 09:40:42 +0000

    Improving code with impl

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,28 @@ impl Rect {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rect) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 fn main() {
-    let rect = Rect {
-        width: 30,
-        height: 50,
+    let rect1: Rect = Rect {
+        width: 310,
+        height: 250,
     };
-    println!("the area of the rectangle is {} square pixels", rect.area());
+    let rect2: Rect = Rect {
+        width: 40,
+        height: 60,
+    };
+    println!(
+        "the area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+    println!(
+        "rect1 {} hold rect2",
+        match rect1.can_hold(&rect2) {
+            true => "can",
+            false => "can't",
+        }
+    )
 }
