7f0c8846f782f8d9923aff9a285fcb4f3c2250bc
revellan <climb.simonschmidt@web.de>
2025-07-25 08:47:14 +0000

    create main program

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,8 @@
+struct Rect(u32,u32);
 fn main() {
+    let rect= Rect(30,50);
+    println!("the area of the rectangle is {} square pixels",area(rect));
+}
+fn area(cords: Rect) -> u32 {
+    cords.0 * cords.1
 }
