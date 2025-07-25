3ee2040554396c42da48de4e5d272e159ffbbc20
revellan <climb.simonschmidt@web.de>
2025-07-25 09:24:40 +0000

    debuggin

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+#[derive(Debug)]
 struct Rect {
     width: u32,
     height: u32,
+}
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 fn main() {
     let rect = Rect {
         width: 30,
         height: 50,
     };
-    println!("the area of the rectangle is {} square pixels", area(&rect));
-}
-fn area(cords: &Rect) -> u32 {
-    cords.width * cords.height
+    println!("the area of the rectangle is {} square pixels", rect.area());
 }
